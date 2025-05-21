27afe2adf344b22082028287118c26c178490c41
zenin5479 <zenin5479@gmail.com>
2025-05-21 19:33:14 +0300

    Update 7.4 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -182,33 +182,9 @@ namespace Homework_4._2
    //{
    //   int n = razmerrowvect();
    //   int m = razmercolumnvect();
-   //   bool fl = false;
-
-   //   double** a = new double*[n];
-
-   //   FILE* fp_a = fopen("a.txt", "r");
-   //   if (fp_a == nullptr)
-   //   {
-   //      printf("Ошибка при открытии файла для чтенияn");
-   //      return 1;
-   //   }
-
    //   vvod_vect(a, n, m, fp_a);
-   //   fclose(fp_a);
-
-   //   double* b;
    //   find_max(b, a, n, m, fl);
-
-   //   FILE* fp_finish = fopen("finish.txt", "w");
-   //   if (fp_finish == nullptr)
-   //   {
-   //      printf("Ошибка при открытии файла для записиn");
-   //      delete[] b;
-   //      return 1;
-   //   }
-
    //   vivod_vector(b, n, fp_finish);
-   //   fclose(fp_finish);
    //}
 
    //void vvod_vect(double**& x, int n, int m, FILE* f)
